172691251ada0bce09b8a86a8f3b839010c937d5
HBKEduardex <eduardex642@gmail.com>
2025-03-17 18:36:37 -0400

    Update leds.py

--- leds.py
+++ leds.py
@@ -1,11 +1,10 @@
 
 import RPi.GPIO as GPIO
 import time
-
 leds = [23, 13, 6]
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(leds, GPIO.OUT)
-
+GPIO.setwarnings(False)
 archivo = "texto_proob2.txt"
 
 def read_interval():
@@ -13,9 +12,13 @@ def read_interval():
         interval = float(file.readline().strip())
         return max(0.1, interval)
 
-while True:
-    interval = read_interval()
-    for led in leds:
-        GPIO.output(led, GPIO.HIGH)
-        time.sleep(interval)
-        GPIO.output(led, GPIO.LOW)
+try:
+    while True:
+        interval = read_interval()
+        for led in leds:
+            GPIO.output(led, GPIO.HIGH)
+            time.sleep(interval)
+            GPIO.output(led, GPIO.LOW)
+except KeyboardInterrupt:
+    print("\nSaliendo del programa...")
+    GPIO.cleanup()
